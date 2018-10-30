00a5550436ee58e1a6675f167e8bf6b0bc2e1b48
Todd Johnston <toddjohnston.career@gmail.com>
2018-10-29 19:57:57 -0700

    Initial commit

---
+++ src/main/java/com/ibexsys/workflow/WorkflowInstallerApplication.java
@@ -0,0 +1,12 @@
+package com.ibexsys.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowInstallerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowInstallerApplication.class, args);
+    }
+}
